4aaeddc5372bd16c3fd3f8230e691d4e0df52697
DanielGarciaS <danieelse17@gmail.com>
2025-11-19 21:33:47 -0600

    Correxion de error de sintaxis.

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 const express = require('express');
-const mongoose = requiere('mongoose');
+const mongoose = require('mongoose');
 const cors = require('cors');
 const connectDB = require('./config/db');
 
